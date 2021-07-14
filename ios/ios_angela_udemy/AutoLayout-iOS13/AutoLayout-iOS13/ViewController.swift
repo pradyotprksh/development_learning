cb4a5f7e66a3e34f5b8ae04ea2984161ed3e7d3d
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2021-07-14 18:50:39 +0530

    Auto constraint added 😅
    - Added auto constraint
    - Updated README.md

--- ios/ios_angela_udemy/AutoLayout-iOS13/AutoLayout-iOS13/ViewController.swift
+++ ios/ios_angela_udemy/AutoLayout-iOS13/AutoLayout-iOS13/ViewController.swift
@@ -9,18 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // Views in the controller
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    // Actions in the controller
+    //
+    // Called when the ROLL button is pressed.
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
         let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-        
         diceImageView1.image = allDice[Int.random(in: 0...5)]
         diceImageView2.image = allDice[Int.random(in: 0...5)]
-        
     }
-    
 }
-
--- ios/ios_angela_udemy/AutoLayout-iOS13/README.md
+++ ios/ios_angela_udemy/AutoLayout-iOS13/README.md
@@ -1,2 +1,5 @@
 # AutoLayout-iOS13 
 
+![auto-layout-2](https://github.com/pradyotprksh/development_learning/blob/main/ios/ios_angela_udemy/Dicee-iOS13/AutoLayout-iOS13_2.png)
+
+![auto-layout-1](https://github.com/pradyotprksh/development_learning/blob/main/ios/ios_angela_udemy/Dicee-iOS13/AutoLayout-iOS13_1.png)
