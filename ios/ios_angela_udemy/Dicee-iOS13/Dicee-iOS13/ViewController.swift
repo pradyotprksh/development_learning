14e516b707093ca18b50fd03fd3f7f9c5fd724e4
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2021-07-14 15:37:26 +0530

    App created 🥳
    - Completed the dice application

--- ios/ios_angela_udemy/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ ios/ios_angela_udemy/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Views for controller
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // Dice number for left and right image
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 1;
+    
+    // Dice images array
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix"),];
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad();
+        
+        updateImages();
     }
-
-
+    
+    // Actions for controller
+    //
+    // When the roll button is clicked.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateImages();
+    }
+    
+    // Update the image of the dices
+    func updateImages() {
+        leftDiceNumber = Int.random(in: 0...5);
+        rightDiceNumber = Int.random(in: 0...5);
+        
+        diceImageView1.image = diceImages[leftDiceNumber];
+        diceImageView2.image = diceImages[rightDiceNumber];
+    }
 }
-
