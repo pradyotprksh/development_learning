174c7403ff3b056ab62d65ea0af1dbf2f452dd44
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2021-07-15 00:55:12 +0530

    Xylophone Project done 🥳
    - Created Xylophone iOS application
    - Updated README.md

--- ios/ios_angela_udemy/Xylophone-iOS13/Xylophone/ViewController.swift
+++ ios/ios_angela_udemy/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // An audio player
+    var player: AVAudioPlayer!;
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
     }
 
-
+    // Actions in controller
+    //
+    // When any button is pressed keyPressed will be called.
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle ?? "C");
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5;
+        
+        //Code should execute after 0.2 second delay.
+         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+             //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0;
+         }
+    }
     
-
+    // Play sound using player
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav");
+        player = try! AVAudioPlayer(contentsOf: url!);
+        player.play();
+    }
 }
-
