ea59b697459d234cbdcbb5c728e99cf2405b2354
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2021-07-22 12:44:25 +0530

    I am rich 🧐
    - Created I am rich app using swift ui

---
+++ ios/angela_udemy/I Am Rich Swift UI/I Am Rich Swift UI/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  I Am Rich Swift UI
+//
+//  Created by Pradyot Prakash on 22/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/angela_udemy/I Am Rich Swift UI/I Am Rich Swift UI/I_Am_Rich_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_Swift_UIApp.swift
+//  I Am Rich Swift UI
+//
+//  Created by Pradyot Prakash on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
