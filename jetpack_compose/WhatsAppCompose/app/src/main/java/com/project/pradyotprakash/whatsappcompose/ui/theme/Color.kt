2d17267b6c5230f93abf23aa27583d8d532635e7
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2021-08-13 19:29:56 +0530

    Whatsapp Clone 🤯
    - Dependencies added
    - Firebase added
    - Changing project structure to https://github.com/android/compose-samples/tree/main/Jetchat

---
+++ jetpack_compose/WhatsAppCompose/app/src/main/java/com/project/pradyotprakash/whatsappcompose/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.project.pradyotprakash.whatsappcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryText = Color(0xFF292929)
+val SecondaryText = Color(0xFF696B6A)
+val Background = Color(0xFFFFFFFF)
+val Action = Color(0xFF075E5F)
+val Notification = Color(0xFF25D366)
+val Success = Color(0xFF34B7F1)
---
+++ jetpack_compose/WhatsAppCompose/app/src/main/java/com/project/pradyotprakash/whatsappcompose/ui/theme/Type.kt
@@ -0,0 +1,36 @@
+package com.project.pradyotprakash.whatsappcompose.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+)
