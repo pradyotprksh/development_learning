2e138f41a3111aa35d0bbb2caf40e5c2f7ea5444
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2021-09-24 10:27:28 +0530

    Sorting 🗒
    - Which sorting algorithms to use?

--- dart/algorithms/bin/sorting.dart
+++ dart/algorithms/bin/sorting.dart
@@ -76,3 +76,19 @@ class Sorting {
     return result;
   }
 }
+
+//#1 - Sort 10 schools around your house by distance: Insertion sort
+
+//#2 - eBay sorts listings by the current Bid amount: Merge / Radix / Counting sort
+
+//#3 - Sport scores on ESPN : Merge / Quick sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data - Quick sort / Merge Sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews - Quick sort / Insertion sort
+
+//#6 - Temperature Records for the past 50 years in Canada - Quick / Radix / Counting sort
+
+//#7 - Large user name database needs to be sorted. Data is very random. - Merge / Quick sort
+
+//#8 - You want to teach sorting for the first time - Bubble sort
