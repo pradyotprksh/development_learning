ba0e68ce9f9bdda073396d5af828acf4344a4bc0
Pradyot Prakash <pradyotprksh4@gmail.com>
2022-02-08 22:55:07 +0530

    Jetpack Compose 💚
    - Removed kinstagram for now

--- jetpack_compose/Kintagram/buildSrc/src/main/kotlin/GradleLibraries.kt
+++
@@ -1,7 +0,0 @@
-/**
- * A set of gradle libraries which will be used throughout the application
- */
-object GradleLibraries {
-    const val hiltPlugin =
-        "com.google.dagger:hilt-android-gradle-plugin:${Versions.Plugins.hiltPlugin}"
-}
