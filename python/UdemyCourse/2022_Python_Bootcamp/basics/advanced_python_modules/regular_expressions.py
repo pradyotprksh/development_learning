dcef908a54588d542c4d94390809ed364ef43b14
Pradyot Prakash <pradyotprksh4@gmail.com>
2022-02-08 23:34:29 +0530

    Python 🐍
    - Regular expressions part 3

--- python/UdemyCourse/2022_Python_Bootcamp/basics/advanced_python_modules/regular_expressions.py
+++ python/UdemyCourse/2022_Python_Bootcamp/basics/advanced_python_modules/regular_expressions.py
@@ -44,6 +44,44 @@ def part_2():
     log_debug(results.group(3))
 
 
+def part_3():
+    log_debug(re.search(r'cat', 'The cat is here'))
+    log_debug(re.search(r'dog', 'The cat is here'))
+    log_debug(re.search(r'cat | dog', 'The cat is here'))
+    for match in re.finditer(r'cat|dog', 'The cat & dog is here'):
+        log_debug(match)
+
+    log_debug(re.findall(r'.{3}at', 'The cat in the hat when splat'))
+
+    log_debug(re.findall(r'^\d', '1 is a number'))
+    log_debug(re.findall(r'^\d', 'The 1 is a number'))
+
+    log_debug(re.findall(r'\d$', '1 is a number'))
+    log_debug(re.findall(r'\d$', 'The number is 2'))
+
+    phrase = 'There are 3 numbers 34 inside 5 this sentence'
+    pattern = r'[^\d]'
+    log_debug(re.findall(pattern, phrase))
+    pattern = r'[^\d]+'
+    log_debug(re.findall(pattern, phrase))
+    phrase = 'This a string. But how can we remove punctuations? Please help!!'
+    pattern = r'[^!.? ]+'
+    clean = re.findall(pattern, phrase)
+    log_debug(' '.join(clean))
+
+    text = 'Only find the hyphen-words ih this sentence. But you do not know how long-ish they are'
+    pattern = r'[\w]+-[\w]+'
+    log_debug(re.findall(pattern, text))
+
+    test1 = "This is a catfish"
+    test2 = "This is a catnap"
+    test3 = "This is a catwork"
+    log_debug(re.search(r'cat(fish|nap|work)', test1))
+    log_debug(re.search(r'cat(fish|nap|work)', test2))
+    log_debug(re.search(r'cat(fish|nap|claw)', test3))
+    log_debug(re.search(r'cat(fish|nap|work)', test3))
+
+
 def regular_expressions():
     """
     Let's take a look on regular expressions in Python
@@ -52,3 +90,4 @@ def regular_expressions():
 
     part_1()
     part_2()
+    part_3()
