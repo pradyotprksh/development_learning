38a581a3b3e04d232e917c579ef1f70530259415
Pradyot Prakash <pradyotprksh4@gmail.com>
2022-02-08 23:16:10 +0530

    Python 🐍
    - Regular expressions part 2

--- python/UdemyCourse/2022_Python_Bootcamp/basics/advanced_python_modules/regular_expressions.py
+++ python/UdemyCourse/2022_Python_Bootcamp/basics/advanced_python_modules/regular_expressions.py
@@ -2,12 +2,7 @@ from src import log_debug
 import re
 
 
-def regular_expressions():
-    """
-    Let's take a look on regular expressions in Python
-    :return: None
-    """
-
+def part_1():
     text = "Contact me using the phone number 123-123-123. Call soon!!"
     log_debug("phone" in text)
 
@@ -30,3 +25,30 @@ def regular_expressions():
     log_debug(matches)
     for match in re.finditer("phone", text):
         log_debug(match)
+
+
+def part_2():
+    text = "My phone number is 123-456-7890"
+    phone = re.search(r'\d\d\d-\d\d\d-\d\d\d\d', text)
+    log_debug(phone.group())
+
+    phone = re.search(r'\d{3}-\d{3}-\d{4}', text)
+    log_debug(phone.group())
+
+    phone_pattern = re.compile(r'(\d{3})-(\d{3})-(\d{4})')
+    results = re.search(phone_pattern, text)
+    log_debug(results.group())
+    # Group order starts from one
+    log_debug(results.group(1))
+    log_debug(results.group(2))
+    log_debug(results.group(3))
+
+
+def regular_expressions():
+    """
+    Let's take a look on regular expressions in Python
+    :return: None
+    """
+
+    part_1()
+    part_2()
