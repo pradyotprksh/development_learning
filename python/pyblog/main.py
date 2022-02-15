02585018b7373c8671d46376e96232db36bb79ae
Pradyot Prakash <pradyotprksh4@gmail.com>
2022-02-15 20:53:38 +0530

    PyBlog 🐍
    - tkinter not working properly in mac
    - will check django

--- python/pyblog/main.py
+++ python/pyblog/main.py
@@ -1,22 +1,5 @@
 """PyBlog"""
 
 
-import tkinter as tk
-from tkinter import ttk
-from src import Constants
-
-
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.title(Constants.Variables.PROJECT_NAME)
-
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    # Full Screen
-    root.geometry(f"{screen_width}x{screen_height}+{0}+{0}")
-    # TODO: Project icon doesn't show
-    root.iconbitmap(Constants.Paths.MAIN_ICON)
-
-    ttk.Label(master=root, text="PyBlog").pack()
-    root.mainloop()
+    pass
