529c681c65425bd1cd5d6e83b54699cde6b8bbfc
Pradyot Prakash <pradyotprksh4@gmail.com>
2022-04-22 16:56:58 +0530

    Second Hand Clothes 💙
    - Fix action issues

--- flutter/second_hand_clothes/lib/app/themes/bloc/theme_state.dart
+++ flutter/second_hand_clothes/lib/app/themes/bloc/theme_state.dart
@@ -1,7 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:second_hand_clothes/app/app.dart';
 
 /// A theme state for the application. This will contain the current
