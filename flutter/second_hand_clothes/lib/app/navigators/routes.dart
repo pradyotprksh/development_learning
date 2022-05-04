ae96d4956aec16fe4db3038a520742ac25f9a8ed
Pradyot Prakash <pradyotprksh4@gmail.com>
2022-05-04 18:39:00 +0530

    Second Hand Clothes 💙
    - Code refactored

--- flutter/second_hand_clothes/lib/app/navigators/routes.dart
+++ flutter/second_hand_clothes/lib/app/navigators/routes.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:second_hand_clothes/app/app.dart';
-import 'package:second_hand_clothes/domain/domain.dart';
 
 /// A routes class which will contain all the details related to routes
 /// which are being taken in the applicaiton.
