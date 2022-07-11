902b453dac6cd685406e01b069c126d006db7ec1
Pradyot Prakash <pradyotprksh4@gmail.com>
2022-07-11 19:05:39 +0530

    Python 🐍
    - Fix lint

--- python/ds_algo/README.md
+++ python/ds_algo/README.md
@@ -1,1 +1,5 @@
 # Data Structures and Algorithm
+
+To run pylint 
+
+`pylint setup.py & pylint main.py & pylint __init__.py & pylint ./practice`
--- python/ds_algo/main.py
+++ python/ds_algo/main.py
@@ -1,4 +1,5 @@
-from practice import practice
+"""Main file to be run"""
+import practice
 
 if __name__ == '__main__':
     practice.run_practice_problems()
