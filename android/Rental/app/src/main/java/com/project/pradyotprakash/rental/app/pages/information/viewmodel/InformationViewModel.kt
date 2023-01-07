07cd6cfd9a7babea1bd2980ce0784936eb6a31c1
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-01-07 15:37:41 +0530

    Android 💚
    - Code refactored

--- android/Rental/app/src/main/java/com/project/pradyotprakash/rental/app/pages/information/viewmodel/InformationViewModel.kt
+++ android/Rental/app/src/main/java/com/project/pradyotprakash/rental/app/pages/information/viewmodel/InformationViewModel.kt
@@ -270,7 +270,7 @@ class InformationViewModel @Inject constructor(
                         permanentAddress!!,
                         emailAddress!!,
                         userType!!,
-                        profilePicUrl!!,
+                        profilePicUrl,
                     )
                 }
             )
