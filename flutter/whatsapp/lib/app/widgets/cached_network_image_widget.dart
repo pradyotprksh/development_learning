5a4fe40007cf858415ae586208bd841e336e01a7
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-02-03 15:43:20 +0530

    Flutter 💙
    - Remove hero tag for now

--- flutter/whatsapp/lib/app/widgets/cached_network_image_widget.dart
+++ flutter/whatsapp/lib/app/widgets/cached_network_image_widget.dart
@@ -1,18 +1,17 @@
-import 'dart:math';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CachedNetworkImageWidget extends StatelessWidget {
-  const CachedNetworkImageWidget(
-      {super.key,
-      required this.imageUrl,
-      required this.placeholder,
-      required this.width,
-      required this.height,
-      this.showProgressIndicator = false,
-      this.clipToCircle = false,
-      this.tag});
+  const CachedNetworkImageWidget({
+    super.key,
+    required this.imageUrl,
+    required this.placeholder,
+    required this.width,
+    required this.height,
+    this.showProgressIndicator = false,
+    this.clipToCircle = false,
+    this.tag,
+  });
 
   final String imageUrl;
   final Widget placeholder;
@@ -23,31 +22,28 @@ class CachedNetworkImageWidget extends StatelessWidget {
   final String? tag;
 
   @override
-  Widget build(BuildContext context) => Hero(
-        tag: tag ?? Random.secure().toString(),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            if (imageUrl.isNotEmpty)
-              ClipRRect(
-                borderRadius: clipToCircle
-                    ? BorderRadius.circular(height)
-                    : BorderRadius.zero,
-                child: CachedNetworkImage(
-                  imageUrl: imageUrl,
-                  placeholder: (_, __) => placeholder,
-                  width: width,
-                  height: height,
-                ),
-              ),
-            if (imageUrl.isEmpty) placeholder,
-            if (showProgressIndicator)
-              SizedBox(
-                height: height,
+  Widget build(BuildContext context) => Stack(
+        alignment: Alignment.center,
+        children: [
+          if (imageUrl.isNotEmpty)
+            ClipRRect(
+              borderRadius: clipToCircle
+                  ? BorderRadius.circular(height)
+                  : BorderRadius.zero,
+              child: CachedNetworkImage(
+                imageUrl: imageUrl,
+                placeholder: (_, __) => placeholder,
                 width: width,
-                child: const CircularProgressIndicator(),
+                height: height,
               ),
-          ],
-        ),
+            ),
+          if (imageUrl.isEmpty) placeholder,
+          if (showProgressIndicator)
+            SizedBox(
+              height: height,
+              width: width,
+              child: const CircularProgressIndicator(),
+            ),
+        ],
       );
 }
