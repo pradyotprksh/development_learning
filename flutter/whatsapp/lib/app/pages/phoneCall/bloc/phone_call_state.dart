e30ea003d99e5d2774c793ccc947666704912cb4
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-02-04 14:31:05 +0530

    Flutter 💙
    - Updated phone state

--- flutter/whatsapp/lib/app/pages/phoneCall/bloc/phone_call_state.dart
+++ flutter/whatsapp/lib/app/pages/phoneCall/bloc/phone_call_state.dart
@@ -4,6 +4,9 @@ enum CallState {
   idle,
   ringing,
   ongoing,
+  missed,
+  canceled,
+  busy,
 }
 
 enum SpeakerState {
@@ -21,22 +24,26 @@ class PhoneCallState extends Equatable {
     this.callState = CallState.idle,
     this.speakerState = SpeakerState.notOnSpeaker,
     this.muteState = MuteState.unMute,
+    this.documentIds = const [],
   });
 
   PhoneCallState copyWith({
     CallState? callState,
     SpeakerState? speakerState,
     MuteState? muteState,
+    List<String>? documentIds,
   }) =>
       PhoneCallState(
         callState: callState ?? this.callState,
         speakerState: speakerState ?? this.speakerState,
         muteState: muteState ?? this.muteState,
+        documentIds: documentIds ?? this.documentIds,
       );
 
   final CallState callState;
   final SpeakerState speakerState;
   final MuteState muteState;
+  final List<String> documentIds;
 
   @override
   List<Object?> get props => [
