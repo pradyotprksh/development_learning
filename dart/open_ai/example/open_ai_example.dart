b73993261c5cc4d7d5086a2e3c70a6483604b81f
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-03-02 14:05:50 +0530

    Dart 💙
    - Added packages
    - Code cleanup

--- dart/open_ai/example/open_ai_example.dart
+++ dart/open_ai/example/open_ai_example.dart
@@ -1,6 +1,1 @@
-import 'package:open_ai/open_ai.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- dart/open_ai/test/open_ai_test.dart
+++ dart/open_ai/test/open_ai_test.dart
@@ -1,16 +1,1 @@
-import 'package:open_ai/open_ai.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+void main() {}
