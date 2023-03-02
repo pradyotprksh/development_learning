6ac6365b13dbc3f084182fd3f8322085038ab5ce
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-03-02 18:07:14 +0530

    Flutter 💙
    - Working on menu option

--- flutter/whatsapp/lib/app/pages/messageDetails/view/message_details_view.dart
+++ flutter/whatsapp/lib/app/pages/messageDetails/view/message_details_view.dart
@@ -7,14 +7,72 @@ class MessageDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final details = context.routeSettings?.arguments as MessageRouteDetails?;
-    if (details != null) {
-      context.read<MessageDetailsBloc>().add(FetchDetails(details));
-    }
+    final details = context.routeSettings?.arguments as MessageRouteDetails;
+    context.read<MessageDetailsBloc>().add(FetchDetails(details));
 
     return Scaffold(
       backgroundColor: context.themeData.scaffoldBackgroundColor,
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          PopupMenuButton<MessageDetailsMenuItem>(
+            onSelected: (item) {
+              switch (item) {
+                case MessageDetailsMenuItem.share:
+                  break;
+                case MessageDetailsMenuItem.edit:
+                  break;
+                case MessageDetailsMenuItem.viewInAddressBook:
+                  break;
+                case MessageDetailsMenuItem.viewSecurityCode:
+                  break;
+                case MessageDetailsMenuItem.changeSubject:
+                  break;
+              }
+            },
+            color: context.themeData.popupMenuTheme.color,
+            itemBuilder: (_) => [
+              if (details.isDirectMessage)
+                PopupMenuItem(
+                  value: MessageDetailsMenuItem.edit,
+                  child: Text(
+                    context.translator.viewContact,
+                  ),
+                ),
+              if (details.isGroup)
+                PopupMenuItem(
+                  value: MessageDetailsMenuItem.changeSubject,
+                  child: Text(
+                    context.translator.mediaLinksDocs,
+                  ),
+                ),
+              if (details.isDirectMessage)
+                PopupMenuItem(
+                  value: MessageDetailsMenuItem.share,
+                  child: Text(
+                    context.translator.search,
+                  ),
+                ),
+              if (details.isDirectMessage)
+                PopupMenuItem(
+                  value: MessageDetailsMenuItem.viewInAddressBook,
+                  child: Text(
+                    context.translator.muteNotifications,
+                  ),
+                ),
+              if (details.isDirectMessage)
+                PopupMenuItem(
+                  value: MessageDetailsMenuItem.viewSecurityCode,
+                  child: Text(
+                    context.translator.disappearingMessages,
+                  ),
+                ),
+            ],
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
