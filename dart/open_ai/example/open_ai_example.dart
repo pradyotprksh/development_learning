96c68f8fed5568c2a1c6fd09801a9a4c90a1aa5c
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-03-02 13:32:30 +0530

    Flutter 💙
    - Created open ai project

---
+++ dart/open_ai/example/open_ai_example.dart
@@ -0,0 +1,6 @@
+import 'package:open_ai/open_ai.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart/open_ai/test/open_ai_test.dart
@@ -0,0 +1,16 @@
+import 'package:open_ai/open_ai.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
