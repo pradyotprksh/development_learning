f5f7b6b872894624693e93f5386a961ba1d69366
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-03-20 13:21:08 +0530

    Go 🐼
    - Created even or odd
    - Actions updated

---
+++ go/stephen_udemy/evenodd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, num := range nums {
+		if num%2 == 0 {
+			fmt.Printf("%v is even\n", num)
+		} else {
+			fmt.Printf("%v is odd\n", num)
+		}
+	}
+}
