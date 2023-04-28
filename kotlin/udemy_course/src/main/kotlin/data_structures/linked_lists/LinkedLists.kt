e75d75aec7e0b39473cb88b9826000bdae195190
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-04-28 14:02:23 +0530

    Kotlin 🪁
    - Learning linked list

--- kotlin/udemy_course/src/main/kotlin/data_structures/linked_lists/LinkedLists.kt
+++ kotlin/udemy_course/src/main/kotlin/data_structures/linked_lists/LinkedLists.kt
@@ -1,6 +1,8 @@
 package data_structures.linked_lists
 
-data class Node(val data: Int, var next: Node? = null) {
+import java.lang.IllegalArgumentException
+
+data class Node(var data: Int, var next: Node? = null) {
     override fun toString(): String {
         return "$data->$next"
     }
@@ -20,7 +22,32 @@ class LinkedLists {
         singlyLinkedList.push(3)
         singlyLinkedList.print()
         singlyLinkedList.pop()
-        singlyLinkedList.pop()
+        singlyLinkedList.print()
+        singlyLinkedList.shift()
+        singlyLinkedList.print()
+        singlyLinkedList.unshift(4)
+        singlyLinkedList.unshift(5)
+        singlyLinkedList.print()
+        singlyLinkedList.push(6)
+        singlyLinkedList.print()
+        println(singlyLinkedList.get(0)?.data)
+        println(singlyLinkedList.get(3)?.data)
+        singlyLinkedList.set(0, 7)
+        singlyLinkedList.set(3, 8)
+        singlyLinkedList.print()
+        singlyLinkedList.insert(0, 9)
+        singlyLinkedList.print()
+        singlyLinkedList.insert(4, 10)
+        singlyLinkedList.print()
+        singlyLinkedList.insert(3, 11)
+        singlyLinkedList.print()
+        singlyLinkedList.remove(0)
+        singlyLinkedList.print()
+        singlyLinkedList.remove(4)
+        singlyLinkedList.print()
+        singlyLinkedList.remove(2)
+        singlyLinkedList.print()
+        singlyLinkedList.reverse()
         singlyLinkedList.print()
     }
 }
@@ -49,7 +76,7 @@ class SinglyLinkedList {
         if (tail == null) {
             throw IllegalStateException("Linked List is Empty")
         }
-        val data = tail?.data;
+        val data = tail?.data
         if (head == tail) {
             head = null
             tail = null
@@ -58,11 +85,158 @@ class SinglyLinkedList {
             while (temp?.next != tail) {
                 temp = temp?.next
             }
-            temp?.next = null
             tail = temp
+            tail?.next = null
         }
         --length
         return data
+    }
+
+    fun shift(): Int? {
+        if (head == null) {
+            throw IllegalStateException("Linked List is Empty")
+        }
+        val data = head?.data
+        head = head?.next
+        --length
+        return data
+    }
+
+    fun unshift(data: Int) {
+        val node = Node(data = data)
+        node.next = head
+        head = node
+        if (tail == null) {
+            tail = head
+        }
+        ++length
+    }
+
+    fun get(pos: Int): Node? {
+        if (head == null) {
+            throw IllegalStateException("Linked List is Empty")
+        }
+        if (pos < 0 || pos >= length) {
+            throw IllegalArgumentException("Position should be >= 0 and <= $length")
+        }
+
+        var tempPos = pos
+        var temp = head
+        while (tempPos > 0 && temp != null) {
+            temp = temp.next
+            --tempPos
+        }
+
+        return temp
+    }
+
+    fun set(pos: Int, data: Int) {
+        if (head == null) {
+            throw IllegalStateException("Linked List is Empty")
+        }
+        if (pos < 0 || pos >= length) {
+            throw IllegalArgumentException("Position should be >= 0 and <= $length")
+        }
+
+        var tempPos = pos
+        var temp = head
+        while (tempPos > 0 && temp != null) {
+            temp = temp.next
+            --tempPos
+        }
+
+        temp?.data = data
+    }
+
+    fun insert(pos: Int, data: Int) {
+        if (head == null) {
+            throw IllegalStateException("Linked List is Empty")
+        }
+        if (pos < 0 || pos >= length) {
+            throw IllegalArgumentException("Position should be >= 0 and <= $length")
+        }
+
+        when (pos) {
+            0 -> {
+                unshift(data = data)
+            }
+            length - 1 -> {
+                push(data = data)
+            }
+            else -> {
+                val node = Node(data = data)
+
+                var tempPos = pos
+                var temp = head
+                var prev: Node? = null
+
+                while (tempPos > 0 && temp != null) {
+                    prev = temp
+                    temp = temp.next
+                    --tempPos
+                }
+
+                node.next = temp
+                prev?.next = node
+                ++length
+            }
+        }
+    }
+
+    fun remove(pos: Int): Int? {
+        if (head == null) {
+            throw IllegalStateException("Linked List is Empty")
+        }
+        if (pos < 0 || pos >= length) {
+            throw IllegalArgumentException("Position should be >= 0 and <= $length")
+        }
+
+        when (pos) {
+            0 -> {
+                return shift()
+            }
+            length - 1 -> {
+                return pop()
+            }
+            else -> {
+                var tempPos = pos
+                var temp = head
+                var prev: Node? = null
+
+                while (tempPos > 0 && temp != null) {
+                    prev = temp
+                    temp = temp.next
+                    --tempPos
+                }
+
+                prev?.next = temp?.next
+                temp?.next = null
+
+                --length
+
+                return temp?.data
+            }
+        }
+    }
+
+    fun reverse() {
+        if (head == null) {
+            throw IllegalStateException("Linked List is Empty")
+        }
+
+        var temp = head
+        head = tail
+        tail = temp
+
+        var next = temp?.next
+        var prev: Node? = null
+
+        while (temp != null) {
+            temp.next = prev
+            prev = temp
+            temp = next
+            next = next?.next
+        }
     }
 
     fun print() {
--- kotlin/udemy_course/src/test/kotlin/data_structures/linked_lists/SinglyLinkedListTest.kt
+++ kotlin/udemy_course/src/test/kotlin/data_structures/linked_lists/SinglyLinkedListTest.kt
@@ -4,6 +4,7 @@ import junit.framework.TestCase
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.lang.Exception
 import java.lang.IllegalArgumentException
 
 @RunWith(JUnit4::class)
@@ -36,8 +37,6 @@ class SinglyLinkedListTest : TestCase() {
 
     @Test(expected = IllegalStateException::class)
     fun pop() {
-        singlyLinkedList.pop()
-
         assertEquals(0, singlyLinkedList.length)
 
         singlyLinkedList.push(1)
@@ -51,5 +50,111 @@ class SinglyLinkedListTest : TestCase() {
         assertEquals(1, singlyLinkedList.length)
         assertTrue(singlyLinkedList.pop() == 1)
         assertEquals(0, singlyLinkedList.length)
+        singlyLinkedList.pop()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun shift() {
+        assertEquals(0, singlyLinkedList.length)
+
+        singlyLinkedList.push(1)
+        singlyLinkedList.push(2)
+        singlyLinkedList.push(3)
+
+        assertEquals(3, singlyLinkedList.length)
+        assertTrue(singlyLinkedList.shift() == 1)
+        assertEquals(2, singlyLinkedList.length)
+        assertTrue(singlyLinkedList.shift() == 2)
+        assertEquals(1, singlyLinkedList.length)
+        assertTrue(singlyLinkedList.shift() == 3)
+        assertEquals(0, singlyLinkedList.length)
+        singlyLinkedList.shift()
+    }
+
+    @Test
+    fun unshift() {
+        assertEquals(0, singlyLinkedList.length)
+
+        singlyLinkedList.unshift(1)
+        assertEquals(singlyLinkedList.head?.data, singlyLinkedList.tail?.data)
+        assertEquals(1, singlyLinkedList.length)
+
+        singlyLinkedList.unshift(2)
+        assertTrue(singlyLinkedList.head?.data == 2)
+        assertTrue(singlyLinkedList.tail?.data == 1)
+        assertEquals(2, singlyLinkedList.length)
+    }
+
+    @Test(expected = Exception::class)
+    fun get() {
+        singlyLinkedList.get(0)
+
+        singlyLinkedList.push(1)
+        singlyLinkedList.push(2)
+        singlyLinkedList.push(3)
+
+        assertEquals(1, singlyLinkedList.get(0)?.data)
+        assertEquals(3, singlyLinkedList.get(2)?.data)
+
+        singlyLinkedList.unshift(4)
+
+        assertEquals(4, singlyLinkedList.get(0)?.data)
+    }
+
+    @Test(expected = Exception::class)
+    fun set() {
+        singlyLinkedList.set(0, 1)
+
+        singlyLinkedList.push(1)
+        singlyLinkedList.push(2)
+        singlyLinkedList.push(3)
+        singlyLinkedList.push(4)
+
+        assertEquals(1, singlyLinkedList.head?.data)
+
+        singlyLinkedList.set(0, 5)
+        assertEquals(5, singlyLinkedList.head?.data)
+
+        singlyLinkedList.set(3, 6)
+        assertEquals(6, singlyLinkedList.get(3)?.data)
+        assertEquals(6, singlyLinkedList.tail?.data)
+    }
+
+    @Test(expected = Exception::class)
+    fun insert() {
+        singlyLinkedList.insert(0, 1)
+
+        singlyLinkedList.push(1)
+        singlyLinkedList.insert(0, 2)
+        singlyLinkedList.insert(2, 3)
+
+        assertEquals(2, singlyLinkedList.head?.data)
+        assertEquals(3, singlyLinkedList.tail?.data)
+
+        singlyLinkedList.insert(3, 4)
+        singlyLinkedList.insert(4, 5)
+        singlyLinkedList.insert(6, 6)
+
+        assertEquals(6, singlyLinkedList.tail?.data)
+        assertEquals(4, singlyLinkedList.get(4)?.data)
+    }
+
+    @Test(expected = Exception::class)
+    fun remove() {
+        singlyLinkedList.remove(0)
+
+        singlyLinkedList.push(1)
+        singlyLinkedList.push(2)
+        singlyLinkedList.push(3)
+        singlyLinkedList.push(4)
+        singlyLinkedList.push(5)
+
+        assertEquals(1, singlyLinkedList.remove(0))
+        assertEquals(5, singlyLinkedList.remove(3))
+
+        assertEquals(4, singlyLinkedList.tail?.data)
+        assertEquals(2, singlyLinkedList.head?.data)
+
+        assertEquals(3, singlyLinkedList.remove(1))
     }
 }
