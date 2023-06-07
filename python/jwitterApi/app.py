b0af0848a4d3f8f8a4e5e2354c3274ac3a45b448
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-06-07 14:43:28 +0530

    Python 🐍
    - Updated project
    - Using appwrite for backend

---
+++ python/jwitterApi/app.py
@@ -0,0 +1,19 @@
+"""
+App module
+
+A module for app which holds the details related to Flask and restful APIs.
+
+Also helps in start the application and make it usable by external applications.
+"""
+from flask import Flask
+from flask_restful import Api
+
+# Create flask app object
+app = Flask(__name__)
+# Create the flask restful object
+api = Api(app)
+
+# Below are all the resources used by the application
+
+# Start the application
+app.run(host="0.0.0.0", port=5000, debug=True)
