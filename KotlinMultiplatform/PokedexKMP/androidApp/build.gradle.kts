1c0dcfaa9897822e0ee9f2727a382650dcb21e27
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-08-02 11:17:47 +0530

    KMP 💚
    - Code refactored
    - Libs moved to toml file

--- KotlinMultiplatform/PokedexKMP/androidApp/build.gradle.kts
+++ KotlinMultiplatform/PokedexKMP/androidApp/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 
 android {
     namespace = "com.pradyotprakash.pokedexkmp.android"
+
     compileSdk = 33
+
     defaultConfig {
         applicationId = "com.pradyotprakash.pokedexkmp.android"
         minSdk = 24
@@ -13,37 +15,46 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.7"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
+    // External modules
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.activity:activity-compose:1.7.1")
+
+    // Compose
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.activity.compose)
 }
