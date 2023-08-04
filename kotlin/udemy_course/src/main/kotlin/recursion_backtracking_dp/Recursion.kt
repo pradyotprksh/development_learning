e75dbc509b96c283c32e841bb03ad2c01a491f23
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-08-04 14:24:19 +0530

    Update Recursion.kt

--- kotlin/udemy_course/src/main/kotlin/recursion_backtracking_dp/Recursion.kt
+++ kotlin/udemy_course/src/main/kotlin/recursion_backtracking_dp/Recursion.kt
@@ -17,6 +17,61 @@ class Recursion {
 
         println(reverseString("abc"))
         println(reverseString("Pradyot Prakash"))
+
+        towersOfHanoi(3)
+        towersOfHanoi(6)
+
+        println(fibonacciIterative(5))
+        println(fibonacciIterative(6))
+
+        println(euclideanAlgorithmRecursion(45, 10))
+        println(euclideanAlgorithmRecursion(24, 9))
+        println(euclideanAlgorithmIterative(45, 10))
+        println(euclideanAlgorithmIterative(24, 9))
+    }
+
+    private fun euclideanAlgorithmIterative(a: Int, b: Int): Int {
+        var tempA = a
+        var tempB = b
+
+        while (tempA % tempB != 0) {
+            val temp = tempB
+            tempB = tempA % tempB
+            tempA = temp
+        }
+
+        return tempB
+    }
+
+    private fun euclideanAlgorithmRecursion(a: Int, b: Int): Int {
+        if (a % b == 0) return b
+        return euclideanAlgorithmRecursion(b, a % b)
+    }
+
+    private fun fibonacciIterative(n: Int): Int {
+        var a = 0
+        if (n <= 1) return a
+
+        var b = 1
+
+        for (i in 2..n) {
+            val tempB = b
+            b += a
+            a = tempB
+        }
+
+        return b
+    }
+
+    private fun towersOfHanoi(disk: Int, start: String = "A", middle: String = "B", destination: String = "C") {
+        if (disk == 1) {
+            println("Disk $disk from $start to $destination")
+            return
+        }
+
+        towersOfHanoi(disk - 1, start, destination, middle)
+        println("Disk $disk from $start to $destination")
+        towersOfHanoi(disk - 1, middle, start, destination)
     }
 
     private fun reverseString(s: String): String {
