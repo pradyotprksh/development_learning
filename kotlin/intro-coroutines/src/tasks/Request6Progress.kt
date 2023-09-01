04f8b0b49237cd31b363580578f80b17495b3e33
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-09-01 17:42:15 +0530

    Kotlin 💚
    - Task 6

--- kotlin/intro-coroutines/src/tasks/Request6Progress.kt
+++ kotlin/intro-coroutines/src/tasks/Request6Progress.kt
@@ -1,11 +1,24 @@
 package tasks
 
 import contributors.*
+import java.util.concurrent.atomic.AtomicInteger
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val users = mutableListOf<User>()
+    val incrementCounter = AtomicInteger()
+
+    for (repo in repos) {
+        val user = service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.bodyList()
+        users.addAll(user)
+        updateResults(users.aggregate(), incrementCounter.incrementAndGet() == repos.size)
+    }
 }
