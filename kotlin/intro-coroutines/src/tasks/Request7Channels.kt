cec59f64fbf0b4ae1e195add231d8a62918d7f3a
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-09-01 17:51:46 +0530

    Kotlin 💚
    - Task 7

--- kotlin/intro-coroutines/src/tasks/Request7Channels.kt
+++ kotlin/intro-coroutines/src/tasks/Request7Channels.kt
@@ -11,6 +11,24 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        val channels = Channel<List<User>>()
+        for (repo in repos) {
+            launch {
+                channels.send(
+                    service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.bodyList()
+                )
+            }
+        }
+
+        val users = mutableListOf<User>()
+        repeat(repos.size) {
+            users.addAll(channels.receive())
+            updateResults(users.aggregate(), it == repos.lastIndex)
+        }
     }
 }
