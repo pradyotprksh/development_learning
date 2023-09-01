bd6666cce7da662bd9381446064674f953469666
Pradyot Prakash <pradyotprksh4@gmail.com>
2023-09-01 18:14:11 +0530

    Kotlin 💚
    - Task 8

--- kotlin/intro-coroutines/test/tasks/Request5ConcurrentKtTest.kt
+++ kotlin/intro-coroutines/test/tasks/Request5ConcurrentKtTest.kt
@@ -4,24 +4,23 @@ import contributors.MockGithubService
 import contributors.expectedConcurrentResults
 import contributors.testRequestData
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
 
 class Request5ConcurrentKtTest {
     @Test
-    fun testConcurrent() = runBlocking {
-        val startTime = System.currentTimeMillis()
+    fun testConcurrent() = runTest {
+        val startTime = currentTime
         val result = loadContributorsConcurrent(MockGithubService, testRequestData)
         Assert.assertEquals("Wrong result for 'loadContributorsConcurrent'", expectedConcurrentResults.users, result)
-        val totalTime = System.currentTimeMillis() - startTime
-        /*
-        // TODO: uncomment this assertion
+        val totalTime = currentTime - startTime
         Assert.assertEquals(
             "The calls run concurrently, so the total virtual time should be 2200 ms: " +
                     "1000 ms for repos request plus max(1000, 1200, 800) = 1200 ms for concurrent contributors requests)",
             expectedConcurrentResults.timeFromStart, totalTime
         )
-        */
         Assert.assertTrue(
             "The calls run concurrently, so the total virtual time should be 2200 ms: " +
                     "1000 ms for repos request plus max(1000, 1200, 800) = 1200 ms for concurrent contributors requests)",
