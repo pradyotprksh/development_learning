3bbe901a8e6332c685c58d443ddeb9ca5538751a
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2023-10-31 22:39:07 +0530

    LibraryOwner 💚
    - Working on actions

--- android/Library/Owner/app/src/main/java/com/pradyotprakash/libraryowner/app/pages/details/view/DetailsView.kt
+++ android/Library/Owner/app/src/main/java/com/pradyotprakash/libraryowner/app/pages/details/view/DetailsView.kt
@@ -1,18 +1,22 @@
 package com.pradyotprakash.libraryowner.app.pages.details.view
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pradyotprakash.libraryowner.app.composables.PageStateComposable
 import com.pradyotprakash.libraryowner.app.localization.TR
+import com.pradyotprakash.libraryowner.app.pages.details.view.composables.SectionComposable
 import com.pradyotprakash.libraryowner.app.pages.details.viewmodel.DetailsViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,10 +49,25 @@ fun DetailsView(detailsViewModel: DetailsViewModel = hiltViewModel()) {
                 ),
             ) {
                 item {
-                    Text(
-                        text = "Your Details",
-                        style = MaterialTheme.typography.headlineMedium
-                    )
+                    SectionComposable(
+                        title = TR.yourDetailsTitle,
+                        subtitle = TR.yourDetailsSubtitle
+                    ) {
+                        OutlinedTextField(value = "Name", onValueChange = { value -> })
+                        Spacer(modifier = Modifier.height(5.dp))
+                        OutlinedTextField(value = "Phone Number", onValueChange = { value -> })
+                        Spacer(modifier = Modifier.height(5.dp))
+                        OutlinedTextField(value = "Email Address", onValueChange = { value -> })
+                        Spacer(modifier = Modifier.height(5.dp))
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                item {
+                    SectionComposable(
+                        title = TR.libraryDetailsTitle,
+                        subtitle = TR.libraryDetailsSubtitle
+                    ) {}
+                    Spacer(modifier = Modifier.height(15.dp))
                 }
             }
         }
