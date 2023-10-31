7560fd561231e93dcd16003fc93a5d26784ce5ae
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2023-10-31 22:41:31 +0530

    LibraryOwner 💚
    - Working on actions

--- android/Library/Owner/app/src/main/java/com/pradyotprakash/libraryowner/app/pages/details/viewmodel/DetailsState.kt
+++
@@ -1,9 +0,0 @@
-package com.pradyotprakash.libraryowner.app.pages.details.viewmodel
-
-sealed interface DetailsState {
-    object Idle: DetailsState
-
-    object Loading: DetailsState
-
-    data class Error(val message: String): DetailsState
-}
