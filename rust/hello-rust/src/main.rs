bbf21adb89d2e4de85a8959a40e856be93d6f7d4
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2023-11-10 17:15:49 +0530

    Rust 🛕
    - Learning rust

---
+++ rust/hello-rust/src/main.rs
@@ -0,0 +1,10 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout());
+    say(out, width, &mut writer).unwrap();
+}
