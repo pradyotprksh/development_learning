8f9888b7360095aeadbf5065672bddd3fbb1ccf6
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2023-11-13 15:47:18 +0530

    Rust 🛕
    - Guess number

---
+++ rust/guessing_number/src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please enter your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a valid number");
+                continue;
+            },
+        };
+
+        println!("Your guess is {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+}
