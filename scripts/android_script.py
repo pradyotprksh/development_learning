7ccd70f5c22b9c19a764feb7ab3b5853900ddad8
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-08 17:43:01 +0530

    Jenkins
    - android scripting

--- scripts/android_script.py
+++ scripts/android_script.py
@@ -1,6 +1,15 @@
 import os
+import sys
 
 with open("local.properties","w") as file:
     file.write("sdk.dir=/Users/pradyotprakash/Library/Android/sdk")
 
 os.system("chmod +x ./gradlew")
+
+current_dir = sys.argv[0]
+
+# Google services file check
+google_services_file = ""
+
+print(os.system("pwd"))
+print(os.system(f"{current_dir}"))
