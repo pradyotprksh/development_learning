d7efc0a85e69c8a154793c134ff1fbaba03fd61a
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-08 17:56:24 +0530

    Update android_script.py

--- scripts/android_script.py
+++ scripts/android_script.py
@@ -9,7 +9,11 @@ os.system("chmod +x ./gradlew")
 current_dir = sys.argv[1]
 
 # Google services file check
-google_services_file = ""
+parent_dir = ""
+slash_count = current_dir.count("/")
+for _ in range(slash_count):
+    parent_dir = f"../{parent_dir}"
 
-os.system("pwd")
-print(current_dir)
+google_services_dir = f"{parent_dir}/JenkinsSetup/{current_dir}"
+
+print(google_services_dir)
