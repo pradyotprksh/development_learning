644ac41601302d08cac66f4dd0e6d6a26546f518
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-09 17:21:24 +0530

    Update android_script.py

--- scripts/android_script.py
+++ scripts/android_script.py
@@ -10,20 +10,17 @@ os.system("chmod +x ./gradlew")
 current_dir = sys.argv[1]
 
 # Google services file check
-app_google_service_dir = f"{current_dir}/app"
+app_google_service_dir = f"/android/{current_dir}"
+current_wd = os.getcwd().replace(app_google_service_dir, "")
+app_google_service_dir = f"{current_wd}{app_google_service_dir}/app"
 
 parent_dir = ".."
 slash_count = current_dir.count("/")
 for _ in range(slash_count):
     parent_dir = f"../{parent_dir}"
 
-google_services_dir = f"{parent_dir}/JenkinsSetup/{current_dir}/google-services.json"
-
-print("+++++++++++++++++++")
-os.system("pwd")
-print(f"google_services_dir {google_services_dir} app_google_service_dir {app_google_service_dir}")
-print("+++++++++++++++++++")
+google_services_dir = f"{current_wd}/JenkinsSetup/{current_dir}"
 
 if os.path.isdir(google_services_dir):
-    print(f"Copying google-services.json from {google_services_dir} to {app_google_service_dir}")
-    shutil.copy2(google_services_dir, app_google_service_dir)
+    print(f"Copying google-services.json from {google_services_dir}/google-services.json to {app_google_service_dir}")
+    shutil.copy2(f"{google_services_dir}/google-services.json", app_google_service_dir)
