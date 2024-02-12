adf7f33b8139ad1339698443485691778eac9981
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-12 14:41:51 +0530

    Update jenkins_setup.py

--- scripts/jenkins_setup.py
+++ scripts/jenkins_setup.py
@@ -3,6 +3,7 @@ import os
 if os.path.isdir('JenkinsSetup'):
     print("JenkinsSetup already cloned")
     print("Updating code")
-    os.system("git pull origin")
+    os.system("cd JenkinsSetup")
+    os.system("git fetch origin")
 else:
     os.system("git clone git@github.com:pradyotprksh/JenkinsSetup.git")
