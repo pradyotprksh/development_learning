e77d107734b4dd4c222d7051ddcac607e1dcc069
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-12 15:06:03 +0530

    Update jenkins_setup.py

--- scripts/jenkins_setup.py
+++ scripts/jenkins_setup.py
@@ -4,6 +4,7 @@ import shutil
 if os.path.isdir('JenkinsSetup'):
     os.system("cd JenkinsSetup")
     current_directory = os.getcwd()
+    print(f"Deleting {current_directory}")
     shutil.rmtree(current_directory)
 
 os.system("git clone git@github.com:pradyotprksh/JenkinsSetup.git")
