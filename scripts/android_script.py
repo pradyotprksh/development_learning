10bf3fde127faaa8550492b88fd53643faaf2993
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-13 14:35:15 +0530

    Update android_script.py

--- scripts/android_script.py
+++ scripts/android_script.py
@@ -3,25 +3,36 @@ import shutil
 import subprocess
 import sys
 
+current_dir = sys.argv[1]
+
+def get_jenkins_setup_dir() -> str:
+    android_path = f"/android/{current_dir}"
+    parent_path = os.getcwd().replace(android_path, "")
+    return f"{parent_path}/JenkinsSetup"
+
+def is_directory_avaiable(dir_path) -> bool:
+    return os.path.isdir(dir_path)
+
+def is_file_available(file_path) -> bool:
+    return os.path.isfile(file_path)
+
+def get_local_properties_content() -> str:
+
 def update_local_properties_file():
     with open("local.properties", "w") as file:
         file.write("sdk.dir=/Users/pradyotprakash/Library/Android/sdk")
 
 def add_google_services_file():
-    current_dir = sys.argv[1]
-
-    app_google_service_dir = f"/android/{current_dir}"
-    current_wd = os.getcwd().replace(app_google_service_dir, "")
-    app_google_service_dir = f"{current_wd}{app_google_service_dir}/app"
+    app_google_service_dir = f"{os.getcwd()}/app"
 
     parent_dir = ".."
     slash_count = current_dir.count("/")
     for _ in range(slash_count):
         parent_dir = f"../{parent_dir}"
 
-    google_services_dir = f"{current_wd}/JenkinsSetup/{current_dir}"
+    google_services_dir = f"{get_jenkins_setup_dir()}/{current_dir}"
 
-    if os.path.isdir(google_services_dir):
+    if is_directory_avaiable(google_services_dir):
         print(f"Copying google-services.json to {current_dir}")
         shutil.copy2(f"{google_services_dir}/google-services.json", app_google_service_dir)
 
@@ -46,7 +57,7 @@ def update_release_notes_for_debug():
     commit_message = get_last_commit_message()
     author_name = get_last_commit_author()
 
-    print(f"os.getcwd() {os.getcwd()} commit_message {commit_message} author_name {author_name}")
+    
 
 update_local_properties_file()
 add_google_services_file()
