567a6ae97bb6cc66de2e727cfec5bef87fb20d12
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-18 14:42:37 +0530

    Rust 🛕

--- rust/rustlings/exercises/00_intro/intro1.rs
+++ rust/rustlings/exercises/00_intro/intro1.rs
@@ -13,6 +13,7 @@
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
 
+
 fn main() {
     println!("Hello and");
     println!(r#"       welcome to...                      "#);
