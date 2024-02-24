6b0b2974dd3edf701d0118837e491f4cf0830e09
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-02-24 15:37:03 +0530

    Rust 🛕
    - workflow update

--- rust/udemy_course/mars_calc/src/main.rs
+++ rust/udemy_course/mars_calc/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let mut mars_weight = calculate_weight_on_mars(89.0);
-    mars_weight = mars_weight * 1000.0;
-    println!("Weight on Mars: {}g", mars_weight);
+    let mars_weight = calculate_weight_on_mars(89.0);
+    println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
