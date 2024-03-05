ae004810097aaf7507064933337d01f42f53605c
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-03-05 15:22:23 +0530

    C

---
+++ cs50/c/meow.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void) {
+    int count = 0;
+    while (count < 3) {
+        printf("Meow - While\n");
+        ++count;
+    }
+    for (int i = 0; i < 3; i++) {
+        printf("Meow - For\n");
+    }
+}
