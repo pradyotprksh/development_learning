d2d81da76cb6a9e8f5ae4fc2f5af0c69946fe2c9
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-03-18 13:51:20 +0530

    DS & Algo 🛕
    - Learning ds/algo from https://www.youtube.com/watch?v=2ZLl8GAk1X4

---
+++ rust/ds_algo/src/main.rs
@@ -0,0 +1,8 @@
+mod notes;
+
+fn main() {
+    notes::ds_intro();
+
+    notes::algo_intro();
+    notes::algo_analysis();
+}
---
+++ rust/ds_algo/src/notes.rs
@@ -0,0 +1,30 @@
+pub fn ds_intro() {
+    println!();
+    println!("=*=*=*= DATA STRUCTURES =*=*=*=");
+    println!("- Way to organize data.");
+    println!("-- Helps in processing the data.");
+    println!();
+    println!("Two kinds of data structures:");
+    println!("- Linear");
+    println!("-- Data elements arragned in sequential manner. Each member is connected to previous and next element. Makes easier to traverse them. Eg. Array, Linked List, Stack, Queue, etc.");
+    println!("- Non-Linear");
+    println!("-- Data elements arragned in non-sequential manner. Each member is connected to previous and next element through paths. Traveral is bit difficult and not easy to implement. Eg. Tree, Graph, etc.");
+}
+
+pub fn algo_intro() {
+    println!();
+    println!("=*=*=*= ALGORITHMS =*=*=*=");
+    println!("- Set of instructions to perform a task or solve a problem.");
+}
+
+pub fn algo_analysis() {
+    println!();
+    println!("Algorithm analysis helps in finding the best algorithm which runs fast and takes less memory.");
+    println!("Time and space complexity are the two factors which determine the performance of an algorithms.");
+    time_complexity();
+}
+
+fn time_complexity() {
+    println!();
+    println!("Time Complexity ");
+}
