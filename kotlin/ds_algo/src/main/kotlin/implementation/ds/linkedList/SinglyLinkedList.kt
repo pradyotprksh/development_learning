9c3563f018b92069e6c06e438161db890dbaaab6
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-03-27 14:50:56 +0530

    Update SinglyLinkedList.kt

--- kotlin/ds_algo/src/main/kotlin/implementation/ds/linkedList/SinglyLinkedList.kt
+++ kotlin/ds_algo/src/main/kotlin/implementation/ds/linkedList/SinglyLinkedList.kt
@@ -1,11 +1,17 @@
 package implementation.ds.linkedList
 
+import java.lang.IllegalArgumentException
+
 data class SLLNode<T>(
     val data: T,
     var next: SLLNode<T>?
 ) {
     override fun toString(): String {
         return "[$data]->$next"
+    }
+
+    fun onlyNodeString(): String {
+        return "[$data]->..."
     }
 }
 
@@ -16,19 +22,60 @@ class SinglyLinkedListImplementation<T>(
 
     fun append(data: T) {
         val newNode = SLLNode(data = data, next = null)
-        var current = head
-        while (current?.next != null) {
-            current = current.next
+
+        if (head == null) {
+            head = newNode
+        } else {
+            var current = head
+            while (current?.next != null) {
+                current = current.next
+            }
+            current?.next = newNode
         }
-        current?.next = newNode
     }
 
-    fun insert(data: T) {
+    fun insertAtStart(data: T) {
         val newNode = SLLNode(data = data, next = head)
         head = newNode
     }
 
-    private fun length(): Int {
+    fun insertAtPosition(pos: Int, data: T) {
+        if (pos > length()) {
+            throw IllegalArgumentException("Position can't be greater than length")
+        } else if (pos < 0) {
+            throw IllegalArgumentException("Position can't be less than 0")
+        } else {
+            when (pos) {
+                0 -> {
+                    insertAtStart(data = data)
+                }
+
+                length() -> {
+                    append(data = data)
+                }
+
+                else -> {
+                    val newNode = SLLNode(data = data, next = null)
+                    var prevNode = head
+                    var position = 0
+                    while (position < pos - 1) {
+                        prevNode = prevNode?.next
+                        ++position
+                    }
+                    newNode.next = prevNode?.next
+                    prevNode?.next = newNode
+                }
+            }
+        }
+    }
+
+    fun deleteAtStart(): SLLNode<T>? {
+        val temp = head
+        head = head?.next
+        return temp
+    }
+
+    fun length(): Int {
         var count = 0
         var current = head
         while (current != null) {
@@ -46,16 +93,63 @@ class SinglyLinkedListImplementation<T>(
 object SinglyLinkedList {
     fun implementation() {
         val sll = SinglyLinkedListImplementation(data = 1)
+
+        println("Deleting node ${sll.deleteAtStart()?.onlyNodeString()}") // Deleting node [1]->...
+        sll.printSLL() // null. Length: 0
+
+        sll.append(1)
         sll.append(2)
         sll.append(3)
         sll.append(4)
         sll.append(5)
         sll.append(6)
+        sll.printSLL() // [1]->[2]->[3]->[4]->[5]->[6]->null. Length: 6
 
-        sll.printSLL()
+        sll.insertAtStart(7)
+        sll.printSLL() // [7]->[1]->[2]->[3]->[4]->[5]->[6]->null. Length: 7
 
-        sll.insert(7)
+        sll.insertAtPosition(pos = 2, data = 8)
+        sll.printSLL() // [7]->[1]->[8]->[2]->[3]->[4]->[5]->[6]->null. Length: 8
 
-        sll.printSLL()
+        sll.insertAtPosition(pos = 7, data = 9)
+        sll.printSLL() // [7]->[1]->[8]->[2]->[3]->[4]->[5]->[9]->[6]->null. Length: 9
+
+        sll.insertAtPosition(pos = 0, data = 10)
+        sll.printSLL() // [10]->[7]->[1]->[8]->[2]->[3]->[4]->[5]->[9]->[6]->null. Length: 10
+
+        sll.insertAtPosition(pos = sll.length(), data = 11)
+        sll.printSLL() // [10]->[7]->[1]->[8]->[2]->[3]->[4]->[5]->[9]->[6]->[11]->null. Length: 11
+
+        /*
+        sll.insertAtPosition(pos = -1, data = 12)
+
+        Exception in thread "main" java.lang.IllegalArgumentException: Position can't be less than 0
+	    at implementation.ds.linkedList.SinglyLinkedListImplementation.insertAtPosition(SinglyLinkedList.kt:35)
+	    at implementation.ds.linkedList.SinglyLinkedList.implementation(SinglyLinkedList.kt:104)
+	    at MainKt$main$3.invoke(Main.kt:16)
+	    at MainKt$main$3.invoke(Main.kt:16)
+	    at notes.DataStructure.singlyLinkedList(DataStructure.kt:36)
+	    at MainKt.main(Main.kt:16)
+	    at MainKt.main(Main.kt)
+         */
+
+        /*
+        sll.insertAtPosition(pos = sll.length() + 1, data = 12)
+
+        Exception in thread "main" java.lang.IllegalArgumentException: Position can't be greater than length
+	    at implementation.ds.linkedList.SinglyLinkedListImplementation.insertAtPosition(SinglyLinkedList.kt:35)
+        at implementation.ds.linkedList.SinglyLinkedList.implementation(SinglyLinkedList.kt:117)
+        at MainKt$main$3.invoke(Main.kt:16)
+        at MainKt$main$3.invoke(Main.kt:16)
+        at notes.DataStructure.singlyLinkedList(DataStructure.kt:36)
+        at MainKt.main(Main.kt:16)
+        at MainKt.main(Main.kt)
+        */
+
+        println("Deleting node ${sll.deleteAtStart()?.onlyNodeString()}") // Deleting node [10]->...
+        sll.printSLL() // [7]->[1]->[8]->[2]->[3]->[4]->[5]->[9]->[6]->[11]->null. Length: 10
+
+        println("Deleting node ${sll.deleteAtStart()?.onlyNodeString()}") // Deleting node [7]->...
+        sll.printSLL() // [1]->[8]->[2]->[3]->[4]->[5]->[9]->[6]->[11]->null. Length: 10
     }
 }
