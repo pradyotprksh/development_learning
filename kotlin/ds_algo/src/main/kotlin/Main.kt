f358ca49e40c7df255d7e8fd394b89d60d4b1c43
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-03-27 13:09:56 +0530

    Kotlin 💚
    - Updated code

--- kotlin/ds_algo/src/main/kotlin/Main.kt
+++ kotlin/ds_algo/src/main/kotlin/Main.kt
@@ -4,18 +4,13 @@ import notes.Algorithms
 import notes.DataStructure
 
 fun main() {
-    DataStructure.dsIntro()
-
     Algorithms.algoIntro()
-
     Algorithms.algoAnalysis()
-
     Algorithms.timeComplexity { TimeComplexity.timeComplexityExample() }
     Algorithms.spaceComplexity()
-
     Algorithms.asymptoticAnalysisOfAlgorithm()
 
+    DataStructure.dsIntro()
     DataStructure.arrays { Arrays.implementation() }
-
     DataStructure.singlyLinkedList {  }
 }
