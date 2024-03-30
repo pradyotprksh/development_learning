d5a13f369085ca4bdbe5b22fd33d3c430b8641b9
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-03-30 18:59:46 +0530

    Kotlin 💚
    - sum of two list
    - sll done

--- kotlin/ds_algo/src/main/kotlin/implementation/ds/linkedList/SinglyLinkedList.kt
+++ kotlin/ds_algo/src/main/kotlin/implementation/ds/linkedList/SinglyLinkedList.kt
@@ -20,6 +20,60 @@ data class SLLNode<T>(
     }
 }
 
+fun sumOfTwoNumbers(head1: SLLNode<Int>, head2: SLLNode<Int>): SLLNode<Int> {
+    val dummy = SLLNode(data = 0, next = null)
+    var tail: SLLNode<Int>? = dummy
+
+    var tempHead1: SLLNode<Int>? = head1
+    var tempHead2: SLLNode<Int>? = head2
+    var carry = 0
+
+    while (tempHead1 != null && tempHead2 != null) {
+        val sum = tempHead1.data + tempHead2.data + carry
+        val data = sum % 10
+        carry = sum / 10
+        tail?.next = SLLNode(data = data, next = null)
+        tail = tail?.next
+        tempHead1 = tempHead1.next
+        tempHead2 = tempHead2.next
+    }
+
+    if (tempHead1 != null) {
+        tail?.next = SLLNode(data = tempHead1.data + carry, next = tempHead1.next)
+    } else if (tempHead2 != null) {
+        tail?.next = SLLNode(data = tempHead2.data + carry, next = tempHead2.next)
+    }
+
+    return dummy.next ?: dummy
+}
+
+fun <T> mergeTwoSortedList(head1: SLLNode<T>, head2: SLLNode<T>): SLLNode<T> where T: Comparable<T> {
+    val dummy = SLLNode(data = head1.data, next = null)
+    var tail = dummy
+
+    var tempHead1: SLLNode<T>? = head1
+    var tempHead2: SLLNode<T>? = head2
+
+    while (tempHead1 != null && tempHead2 != null) {
+        if (tempHead1.data > tempHead2.data) {
+            tail.next = tempHead2
+            tempHead2 = tempHead2.next
+        } else {
+            tail.next = tempHead1
+            tempHead1 = tempHead1.next
+        }
+        tail = tail.next!!
+    }
+
+    if (tempHead1 != null) {
+        tail.next = tempHead1
+    } else {
+        tail.next = tempHead2
+    }
+
+    return dummy.next ?: dummy
+}
+
 /*
 Sorting insert we can only do on comparable data, so that's why this extension
 will help in calling the insert on sorted linked list only when it's possible
@@ -541,5 +595,72 @@ object SinglyLinkedList {
         println("Remove loop from [1]->[2]->[3]->{1} ${sll.isLooped(node = loop1, removeLoop = true)}. $loop1") // Remove loop from [1]->[2]->[3]->{1} true. [1]->[2]->[3]->null
 
         sll.printSLL() // [1]->[2]->[3]->[4]->[5]->[6]->null. Length: 6
+
+        val head1 = SLLNode(
+            data = 1,
+            next = SLLNode(
+                data = 3,
+                next = SLLNode(
+                    data = 4,
+                    next = SLLNode(
+                        data = 6,
+                        next = null
+                    )
+                )
+            )
+        )
+
+        val head2 = SLLNode(
+            data = 2,
+            next = SLLNode(
+                data = 3,
+                next = SLLNode(
+                    data = 4,
+                    next = SLLNode(
+                        data = 5,
+                        next = SLLNode(
+                            data = 5,
+                            next = SLLNode(
+                                data = 7,
+                                next = SLLNode(
+                                    data = 8,
+                                    next = null
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        print("Merging $head1 + $head2") // Merging [1]->[3]->[4]->[6]->null + [2]->[3]->[4]->[5]->[5]->[7]->[8]->null
+        val mergedSLL = mergeTwoSortedList(head1 = head1, head2 = head2)
+        println(" = $mergedSLL") // = [1]->[2]->[3]->[3]->[4]->[4]->[5]->[5]->[6]->[7]->[8]->null
+
+        val sumHead1 = SLLNode(
+            data = 3,
+            next = SLLNode(
+                data = 4,
+                next = SLLNode(
+                    data = 3,
+                    next = null
+                )
+            )
+        )
+
+        val sumHead2 = SLLNode(
+            data = 5,
+            next = SLLNode(
+                data = 6,
+                next = SLLNode(
+                    data = 4,
+                    next = null
+                )
+            )
+        )
+
+        print("Sum of $sumHead1 + $sumHead2")
+        val sumSLL = sumOfTwoNumbers(head1 = sumHead1, head2 = sumHead2)
+        println(" = $sumSLL")
     }
 }
