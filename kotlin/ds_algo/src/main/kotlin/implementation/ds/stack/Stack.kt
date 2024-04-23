d16a11e7f4baffced0ac28215a55b06afb563d9a
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-04-23 18:35:12 +0530

    Kotlin 💚

--- kotlin/ds_algo/src/main/kotlin/implementation/ds/stack/Stack.kt
+++ kotlin/ds_algo/src/main/kotlin/implementation/ds/stack/Stack.kt
@@ -2,35 +2,35 @@ package implementation.ds.stack
 
 object Stack {
     fun implementation() {
-        val stack = StackLinkedListImplementation(data = 1)
+        val stackLinkedList = StackLinkedListImplementation(data = 1)
 
-        println("Top ${stack.peak()?.onlyNodeString()}") // Top [1]->...
+        println("Top ${stackLinkedList.peak()?.onlyNodeString()}") // Top [1]->...
 
-        println("Pop ${stack.pop()?.onlyNodeString()}") // Pop [1]->...
+        println("Pop ${stackLinkedList.pop()?.onlyNodeString()}") // Pop [1]->...
 
-        println("Top ${stack.peak()?.onlyNodeString()}") /* Stack is empty
+        println("Top ${stackLinkedList.peak()?.onlyNodeString()}") /* Stack is empty
                                                             Top null */
 
-        stack.push(1)
-        stack.push(2)
-        stack.push(3)
+        stackLinkedList.push(1)
+        stackLinkedList.push(2)
+        stackLinkedList.push(3)
 
-        stack.printSLL() // [3]->[2]->[1]->null. Length: 3
+        stackLinkedList.printSLL() // [3]->[2]->[1]->null. Length: 3
 
-        println("Pop ${stack.pop()?.onlyNodeString()}") // Pop [3]->...
-        stack.printSLL() // [2]->[1]->null. Length: 2
-        println("Pop ${stack.pop()?.onlyNodeString()}") // Pop [2]->...
-        stack.printSLL() // [1]->null. Length: 1
-        println("Pop ${stack.pop()?.onlyNodeString()}") // Pop [1]->...
-        stack.printSLL() // null. Length: 0
-        println("Pop ${stack.pop()?.onlyNodeString()}") /* Stack is empty
+        println("Pop ${stackLinkedList.pop()?.onlyNodeString()}") // Pop [3]->...
+        stackLinkedList.printSLL() // [2]->[1]->null. Length: 2
+        println("Pop ${stackLinkedList.pop()?.onlyNodeString()}") // Pop [2]->...
+        stackLinkedList.printSLL() // [1]->null. Length: 1
+        println("Pop ${stackLinkedList.pop()?.onlyNodeString()}") // Pop [1]->...
+        stackLinkedList.printSLL() // null. Length: 0
+        println("Pop ${stackLinkedList.pop()?.onlyNodeString()}") /* Stack is empty
                                                            Pop null */
 
-        stack.push(1)
-        stack.push(2)
-        stack.push(3)
-        stack.printSLL() // [3]->[2]->[1]->null. Length: 3
+        stackLinkedList.push(1)
+        stackLinkedList.push(2)
+        stackLinkedList.push(3)
+        stackLinkedList.printSLL() // [3]->[2]->[1]->null. Length: 3
 
-        println("Top ${stack.peak()?.onlyNodeString()}") // Top [3]->...
+        println("Top ${stackLinkedList.peak()?.onlyNodeString()}") // Top [3]->...
     }
 }
