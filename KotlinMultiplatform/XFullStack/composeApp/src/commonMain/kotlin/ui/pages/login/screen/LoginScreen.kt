599ee358f6a348959f48d31781fe0f3f96aec87f
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-05-03 18:26:36 +0530

    KotlinMultiplatform 💚
    - login screen

--- KotlinMultiplatform/XFullStack/composeApp/src/commonMain/kotlin/ui/pages/login/screen/LoginScreen.kt
+++ KotlinMultiplatform/XFullStack/composeApp/src/commonMain/kotlin/ui/pages/login/screen/LoginScreen.kt
@@ -1,15 +1,44 @@
 package ui.pages.login.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.stringResource
 import ui.composables.XAppBar
 import ui.pages.login.viewModel.LoginViewModel
+import xfullstack.composeapp.generated.resources.Res
+import xfullstack.composeapp.generated.resources.forgot_password
+import xfullstack.composeapp.generated.resources.get_started_login
+import xfullstack.composeapp.generated.resources.next
+import xfullstack.composeapp.generated.resources.phone_email_username
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun LoginScreen(
     loginViewModel: LoginViewModel = viewModel { LoginViewModel() },
@@ -30,5 +59,60 @@ fun LoginScreen(
                 }
             )
         }
-    ) {}
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = it.calculateTopPadding() + 10.dp,
+                    bottom = it.calculateBottomPadding() + 25.dp,
+                    start = it.calculateStartPadding(LocalLayoutDirection.current) + 25.dp,
+                    end = it.calculateEndPadding(LocalLayoutDirection.current) + 25.dp,
+                )
+                .imePadding()
+        ) {
+            Text(
+                stringResource(Res.string.get_started_login),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                label = {
+                    Text(
+                        stringResource(Res.string.phone_email_username)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                HorizontalDivider()
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    OutlinedButton(
+                        onClick = {}
+                    ) {
+                        Text(
+                            stringResource(Res.string.forgot_password)
+                        )
+                    }
+                    Button(
+                        onClick = {}
+                    ) {
+                        Text(
+                            stringResource(Res.string.next)
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
