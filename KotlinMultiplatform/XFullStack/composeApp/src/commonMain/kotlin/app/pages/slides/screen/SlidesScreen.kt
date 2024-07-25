c11b01fb3bdec4af906f390aede1f38eca74b31a
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-07-25 18:54:29 +0530

    KotlinMultiplatform 💚
    - working on key binding dialog

--- KotlinMultiplatform/XFullStack/composeApp/src/commonMain/kotlin/app/pages/slides/screen/SlidesScreen.kt
+++ KotlinMultiplatform/XFullStack/composeApp/src/commonMain/kotlin/app/pages/slides/screen/SlidesScreen.kt
@@ -1,12 +1,15 @@
 package app.pages.slides.screen
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
@@ -19,7 +22,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.composables.LoadingDialogComposable
 import app.pages.slides.viewModel.SlidesViewModel
@@ -85,7 +90,19 @@ fun SlidesScreen(
         },
     ) {
         slidesState.slidesDetails?.let { details ->
-            Column {  }
+            Box {
+                if (details.showKeyMap) {
+                    Box(
+                        modifier = Modifier.align(
+                            Alignment.TopEnd,
+                        ).border(
+                            width = 0.dp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            shape = RoundedCornerShape(10.dp),
+                        )
+                    ) {}
+                }
+            }
         }
     }
 }
