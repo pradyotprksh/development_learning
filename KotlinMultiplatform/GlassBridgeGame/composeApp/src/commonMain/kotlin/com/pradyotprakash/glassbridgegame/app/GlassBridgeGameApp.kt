347f18002c5cb97f67d7a1f5ef8b943600aa3df9
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-10-19 12:25:44 +0530

    KotlinMultiplatform 💚

--- KotlinMultiplatform/GlassBridgeGame/composeApp/src/commonMain/kotlin/com/pradyotprakash/glassbridgegame/app/GlassBridgeGameApp.kt
+++ KotlinMultiplatform/GlassBridgeGame/composeApp/src/commonMain/kotlin/com/pradyotprakash/glassbridgegame/app/GlassBridgeGameApp.kt
@@ -3,7 +3,6 @@ package com.pradyotprakash.glassbridgegame.app
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.pradyotprakash.glassbridgegame.app.navigation.Splash
