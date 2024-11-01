8a581cd7892723de3f54c47f5d4a3b29f674d85a
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2024-11-01 17:40:07 +0530

    KotlinMultiplatform 💚
    - code refactor

--- KotlinMultiplatform/GlassBridgeGame/composeApp/src/commonMain/kotlin/com/pradyotprakash/glassbridgegame/app/pages/bridgeGame/screen/composables/Glass.kt
+++ KotlinMultiplatform/GlassBridgeGame/composeApp/src/commonMain/kotlin/com/pradyotprakash/glassbridgegame/app/pages/bridgeGame/screen/composables/Glass.kt
@@ -7,9 +7,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
@@ -37,20 +37,31 @@ fun Glass(
     )
 
     Box(
-        modifier = modifier.clip(RoundedCornerShape(16.dp))
-            .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f * glassBreakEffect))
-            .blur(10.dp).border(
-                width = 1.dp,
-                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f * glassBreakEffect),
-                shape = RoundedCornerShape(10.dp)
-            ).graphicsLayer(
-                scaleX = glassBreakEffect,
-                scaleY = glassBreakEffect,
-            ).padding(16.dp).height(100.dp).clickable(
-                indication = null,
-                interactionSource = remember { MutableInteractionSource() },
-            ) {
-                onBridgeGlassTap()
-            },
-    ) { }
+        modifier = modifier,
+    ) {
+        glass.player?.let {
+            Player(
+                modifier = Modifier.align(Alignment.Center),
+                player = it,
+                iconSize = 48.dp,
+            )
+        }
+        Box(
+            modifier = Modifier.clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f * glassBreakEffect))
+                .blur(10.dp).border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f * glassBreakEffect),
+                    shape = RoundedCornerShape(10.dp)
+                ).graphicsLayer(
+                    scaleX = glassBreakEffect,
+                    scaleY = glassBreakEffect,
+                ).padding(16.dp).height(100.dp).fillMaxWidth().clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                ) {
+                    onBridgeGlassTap()
+                },
+        ) {}
+    }
 }
--- KotlinMultiplatform/GlassBridgeGame/composeApp/src/commonMain/kotlin/com/pradyotprakash/glassbridgegame/app/pages/bridgeGame/screen/composables/Player.kt
+++ KotlinMultiplatform/GlassBridgeGame/composeApp/src/commonMain/kotlin/com/pradyotprakash/glassbridgegame/app/pages/bridgeGame/screen/composables/Player.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
@@ -12,21 +13,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.pradyotprakash.glassbridgegame.app.pages.bridgeGame.viewModel.state.PlayerState
 
 @Composable
 fun Player(
+    modifier: Modifier = Modifier,
     player: PlayerState,
+    iconSize: Dp = 24.dp,
 ) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier,
     ) {
         Icon(
             Icons.Filled.Person,
             contentDescription = Icons.Filled.Person.name,
             tint = if (player.isThePlayer) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.size(iconSize)
         )
         Spacer(modifier = Modifier.height(2.dp))
         Text(
