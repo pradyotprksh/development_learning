fc95b288dc07bb26d595dffa4f48f185135092a9
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2025-03-23 12:43:27 +0530

    Backend 🔚
    - project upgrade

--- backend/UnitConverter/server/build.gradle.kts
+++ backend/UnitConverter/server/build.gradle.kts
@@ -16,6 +16,4 @@ dependencies {
     implementation(libs.logback)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
-    testImplementation(libs.ktor.server.tests)
-    testImplementation(libs.kotlin.test.junit)
 }
