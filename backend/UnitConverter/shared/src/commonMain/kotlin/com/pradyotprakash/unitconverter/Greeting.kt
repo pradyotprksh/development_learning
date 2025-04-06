8efe747465eb428278bffc3bc51f8d46acfd9fd2
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2025-04-06 18:28:06 +0530

    Backend 🔚
    - running android

--- backend/UnitConverter/shared/src/commonMain/kotlin/com/pradyotprakash/unitconverter/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.pradyotprakash.unitconverter
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
