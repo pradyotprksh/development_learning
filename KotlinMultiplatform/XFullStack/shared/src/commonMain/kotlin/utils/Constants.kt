448d04bb628bdcc42dbf66d8eb531efef1618389
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2025-04-06 13:16:47 +0530

    Backend 🔚 & Android 💚
    - testing server, not working
    - code refactor

--- KotlinMultiplatform/XFullStack/shared/src/commonMain/kotlin/utils/Constants.kt
+++ KotlinMultiplatform/XFullStack/shared/src/commonMain/kotlin/utils/Constants.kt
@@ -3,7 +3,7 @@ package utils
 object Constants {
     object Server {
         const val PORT = 8080
-        const val HOST = "192.168.0.101"
+        const val HOST = "192.168.0.103"
     }
 
     object Database {
--- backend/UnitConverter/server/src/main/kotlin/com/pradyotprakash/unitconverter/config/AppConfig.kt
+++ backend/UnitConverter/server/src/main/kotlin/com/pradyotprakash/unitconverter/config/AppConfig.kt
@@ -1,12 +1,12 @@
 package com.pradyotprakash.unitconverter.config
 
-import com.pradyotprakash.unitconverter.utils.Constants.Server.HOST
-import com.pradyotprakash.unitconverter.utils.Constants.Server.PORT
 import com.pradyotprakash.unitconverter.config.plugins.configureAdministration
 import com.pradyotprakash.unitconverter.config.plugins.configureMonitoring
 import com.pradyotprakash.unitconverter.config.plugins.configureResource
 import com.pradyotprakash.unitconverter.config.plugins.configureRouting
 import com.pradyotprakash.unitconverter.config.plugins.configureSerialization
+import com.pradyotprakash.unitconverter.utils.Constants.Server.HOST
+import com.pradyotprakash.unitconverter.utils.Constants.Server.PORT
 import io.ktor.server.application.Application
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.ApplicationEngineFactory
@@ -14,15 +14,13 @@ import io.ktor.server.engine.BaseApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
-fun setup(): BaseApplicationEngine = server(Netty)
+fun setup() = server(Netty)
 
 private fun server(
     engine: ApplicationEngineFactory<BaseApplicationEngine, out ApplicationEngine.Configuration>,
-): BaseApplicationEngine {
-    return embeddedServer(
-        engine, host = HOST, port = PORT, module = Application::mainModule
-    ).engine
-}
+) = embeddedServer(
+    engine, host = HOST, port = PORT, module = Application::mainModule
+)
 
 fun Application.mainModule() {
     configureResource()
