ace600f177552c4b841e415fc1e3c97263edbbc5
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2025-04-06 18:16:56 +0530

    Update ConversionResponse.kt

--- backend/UnitConverter/shared/src/commonMain/kotlin/com/pradyotprakash/unitconverter/core/models/response/ConversionResponse.kt
+++ backend/UnitConverter/shared/src/commonMain/kotlin/com/pradyotprakash/unitconverter/core/models/response/ConversionResponse.kt
@@ -1,5 +1,8 @@
 package com.pradyotprakash.unitconverter.core.models.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ConversionResponse(
     val value: Double,
     val humanReadable: String,
